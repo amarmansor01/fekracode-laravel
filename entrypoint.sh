d0151a098d7499d1210596e8949d11d524e1bc38
Ammar Mansor <amarmansorhm.01@gmail.com>
2025-10-20 01:57:52 +0300

    Fix Dockerfile: add npm build step and ignore public/build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,11 @@ php artisan route:cache
 php artisan view:cache
 php artisan storage:link
 php artisan migrate --force
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+php artisan cache:clear
+
+
 
 php artisan serve --host 0.0.0.0 --port $PORT
