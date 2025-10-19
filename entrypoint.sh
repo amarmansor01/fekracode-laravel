9a0b3db2d6b7afe61fc1b2f8887ef062fd4a97d2
Ammar Mansor <amarmansorhm.01@gmail.com>
2025-10-20 02:08:31 +0300

    Add built assets (public/build) for deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ php artisan view:clear
 php artisan route:clear
 php artisan cache:clear
 
+php artisan migrate --force
+php artisan db:seed --class=DatabaseSeeder --force
 
 
 php artisan serve --host 0.0.0.0 --port $PORT
