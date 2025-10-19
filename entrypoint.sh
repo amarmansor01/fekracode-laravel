38383f89532ef25ef9d0c0b5f5c496faa85a389a
Ammar Mansor <amarmansorhm.01@gmail.com>
2025-10-20 01:48:57 +0300

    Add built assets (public/build) for deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+php artisan storage:link
 php artisan migrate --force
 
-# شغل السيرفر من مجلد public
 php artisan serve --host 0.0.0.0 --port $PORT
