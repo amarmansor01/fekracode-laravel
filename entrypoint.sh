48f0431f53013167ae7091d5e130b69aaef2ad77
Ammar Mansor <amarmansorhm.01@gmail.com>
2025-10-20 21:56:06 +0300

    إضافة وضبط ملف إعدادات Cloudinary

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ php artisan migrate --force
 php artisan config:clear
 php artisan view:clear
 php artisan route:clear
+php artisan config:clear
 php artisan cache:clear
+php artisan config:clear
 
 php artisan migrate --force
 php artisan db:seed --force
