ce0a91ac155a66fbd8cbabc4ebb47e2d8dd15d04
Ammar Mansor <amarmansorhm.01@gmail.com>
2025-10-21 04:44:39 +0300

    تهيئة Cloudinary من جديد

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Service;
 use Cloudinary\Api\Upload\UploadApi;
+use Cloudinary\Configuration\Configuration;
 
 class ServiceController extends Controller
 {
